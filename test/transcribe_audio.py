c23432f82d4b4d1330c41daa9468d10a40c6b758
Victor Onyeacho <vonyeacho@gmail.com>
2026-03-02 14:27:32 -0600

    Fixed the audio crashing issue

--- test/transcribe_audio.py
+++ test/transcribe_audio.py
@@ -6,49 +6,51 @@ from scipy.io.wavfile import write
 import pyttsx3
 from dotenv import load_dotenv
 import os
+import warnings
+
+warnings.filterwarnings("ignore", category=UserWarning)
 
 # Load environment variables from .env file
-load_dotenv()
+#load_dotenv()
 
 # Get the OpenAI API key
-openai_api_key = os.getenv("OPENAI_API_KEY")
+# openai_api_key = os.getenv("OPENAI_API_KEY")
 
-client = OpenAI(api_key=openai_api_key)
+# client = OpenAI(api_key=openai_api_key)
 
-try:
-    response = client.responses.create(
-        model="gpt-4.1-mini",
-        input="hello",
-        max_output_tokens=20
-    )
+# try:
+#     response = client.responses.create(
+#         model="gpt-4.1-mini",
+#         input="hello",
+#         max_output_tokens=20
+#     )
 
-    print("API key is valid and working!")
-    print("Response:", response.output_text)
+#     print("API key is valid and working!")
+#     print("Response:", response.output_text)
 
-except Exception as e:
-    print(f"API key test failed with error: {e}")
+# except Exception as e:
+#     print(f"API key test failed with error: {e}")
 
 # sample rate
-# sample_rate = 44100
-# seconds = 5
+sample_rate = 44100
+seconds = 5
 
-# # audio
-# print("Recording...")
-# recording = sd.rec(int(seconds * sample_rate), samplerate=sample_rate, channels=1)
-# sd.wait()
+# audio
+print("Recording...")
+recording = sd.rec(int(seconds * sample_rate), samplerate=sample_rate, channels=1)
+sd.wait()
 
-# # saves the audio
-# write("test.wav", sample_rate, np.int16(recording * 32767))
+# saves the audio
+write("test.wav", sample_rate, np.int16(recording * 32767))
 
-# model = whisper.load_model("base")
-# result = model.transcribe("test.wav")
+model = whisper.load_model("base", device="cpu")
+result = model.transcribe("test.wav")
 
-# # prints the transcription result
-# print(result["text"])
+# prints the transcription result
+print(result["text"])
 
-# # speaks the audio
-# engine = pyttsx3.init()
-# engine.say(result["text"])
-# engine.runAndWait()
-
-
+# speaks the audio
+engine = pyttsx3.init()
+engine.say(result["text"])
+engine.runAndWait()
+engine.stop()
